8f9edb91b08daf27f870ec2fd591fd5b050918ff
Aditya Prem Sharma <adityapremsharma@users.noreply.github.com>
2021-01-08 21:20:15 +0530

    add cookies and sessions

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// require('dotenv').config()
+require('dotenv').config()
 const express = require("express")
 const mongoose = require("mongoose")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 // const encrypt = require("mongoose-encryption")
-const md5 = require("md5")
+// const md5 = require("md5")
+// const bcrypt = require("bcrypt")
+
+// const saltRounds = 10
+
+
+const session = require("express-session")
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express()
 
@@ -16,12 +24,25 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"))
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 mongoose.connect("mongodb://localhost:27017/userDB",  { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 // const secret = "Thisisourlittlesecret"
 // const secret = process.env.SECRET
@@ -29,7 +50,10 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema)
 
-
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -43,37 +67,98 @@ app.get("/register", function(req, res) {
     res.render("register")
 })
 
+app.get("/secrets", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("secrets")
+    } else {
+        res.redirect("login")
+    }
+})
+
+app.get("/logout", function(req, res) {
+    req.logout()
+    res.redirect("/")
+})
+
 app.post("/register", function(req, res) {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    newUser.save(function(err) {
-        if(!err) {
-            res.render("secrets")
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            res.redirect("/register")
         } else {
-            res.send(err)
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets")
+            })
         }
     })
+
+
+
+
+
+
+
+
+
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // const newUser = new User({
+    //     email: req.body.username,
+    //     password: hash
+    // })
+
+    // newUser.save(function(err) {
+    //     if(!err) {
+    //         res.render("secrets")
+    //     } else {
+    //         res.send(err)
+    //     }
+    // })
+    // });
+    
 })
 
 app.post("/login", function(req, res) {
-    const username = req.body.username
-    const password = md5(req.body.password)
 
-    User.findOne({email: username}, function(err, foundUser) {
-        if(!err) {
-            if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets")
-                }
-            }
-        } else {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.pasword
+    })
+
+    req.login(user, function(err) {
+        if(err) {
             res.send(err)
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets")
+            })
         }
     })
+
+
+
+
+
+
+
+
+    
+    // const username = req.body.username
+    // const password = req.body.password
+
+    // User.findOne({email: username}, function(err, foundUser) {
+    //     if(!err) {
+    //         if(foundUser) {
+    //             bcrypt.compare(password, foundUser.password, function(err, result) {
+    //             if(result === true) {
+    //                 res.render("secrets")
+    //             }
+    //         });
+                
+    //         }
+    //     } else {
+    //         res.send(err)
+    //     }
+    // })
 })
 
 app.listen(3000, function() {
