c01505aa0ce754da251c332e5f2e69960cb0c9e7
Aditya Prem Sharma <adityapremsharma@users.noreply.github.com>
2021-01-09 23:18:59 +0530

    add OAuth for Google authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser")
 // const encrypt = require("mongoose-encryption")
 // const md5 = require("md5")
 // const bcrypt = require("bcrypt")
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate")
 
 // const saltRounds = 10
 
@@ -39,10 +41,13 @@ mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 // const secret = "Thisisourlittlesecret"
 // const secret = process.env.SECRET
@@ -52,12 +57,44 @@ const User = mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy());
  
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
     res.render("home")
 })
+
+app.get("/auth/google", passport.authenticate("google", {scope: ["profile"]}))
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req, res) {
     res.render("login")
@@ -68,16 +105,34 @@ app.get("/register", function(req, res) {
 })
 
 app.get("/secrets", function(req, res) {
-    if(req.isAuthenticated()) {
-        res.render("secrets")
-    } else {
-        res.redirect("login")
-    }
+    // if(req.isAuthenticated()) {
+    //     res.render("secrets")
+    // } else {
+    //     res.redirect("/login")
+    // }
+
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if(err) {
+            res.send(err)
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers})
+            }
+        }
+    })
 })
 
 app.get("/logout", function(req, res) {
     req.logout()
     res.redirect("/")
+})
+
+app.get("/submit", function(req,res) {
+    if(req.isAuthenticated()) {
+        res.render("submit")
+    } else {
+        res.redirect("/login")
+    }
 })
 
 app.post("/register", function(req, res) {
@@ -91,13 +146,6 @@ app.post("/register", function(req, res) {
             })
         }
     })
-
-
-
-
-
-
-
 
 
     // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
@@ -134,13 +182,6 @@ app.post("/login", function(req, res) {
         }
     })
 
-
-
-
-
-
-
-
     
     // const username = req.body.username
     // const password = req.body.password
@@ -159,6 +200,23 @@ app.post("/login", function(req, res) {
     //         res.send(err)
     //     }
     // })
+})
+
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            res.send(err)
+        } else {
+            if(foundUser) {
+                foundUser.secret = submittedSecret
+                foundUser.save(function() {
+                    res.redirect("/secrets")
+                })
+            }
+        }
+    })
 })
 
 app.listen(3000, function() {
